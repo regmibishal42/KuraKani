ae3cf8367572010dea4c42df23d3a3b59b1112f3
Bishal Regmi <regmibishal42@gmail.com>
2022-07-26 14:41:36 +0545

    Setting up frontend backend and database

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const chats = require('./data/data.js');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+dotenv.config();
+
+app.get('/',(req,res)=>{
+    res.send('API is running');
+});
+
+app.get('/api/chats',(req,res)=>{
+    res.send(chats);
+});
+
+app.get('/api/chat/:id',(req,res)=>{
+    res.send('Unique Id Chat',req.params.id);
+});
+const port = process.env.PORT || 3000;
+
+app.listen(3000,console.log(`Server is Listening on port ${port}`));
