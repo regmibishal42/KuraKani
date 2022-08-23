4ee32edce6b5da2400dfb5abd3a564bcbb594dc8
Bishal Regmi <regmibishal42@gmail.com>
2022-08-23 10:50:12 +0545

    fuck this shit4

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ const __dirname1 = path.resolve();
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname1, "/frontend/dist/kurakani")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist/kurakani", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
