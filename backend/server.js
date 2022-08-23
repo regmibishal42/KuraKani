4c2d87466cedcb13abfe8b1f980e394d136b4eed
Bishal Regmi <regmibishal42@gmail.com>
2022-08-23 10:43:16 +0545

    fuck this shit3

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const chatRoutes = require('./routes/chatRoutes');
 const messageRoute = require('./routes/messageRoutes');
 const {notFound,errorHandler} = require('./middleware/errorMiddleware');
 const path = require('path');
-const { dirname } = require('path');
 
 const app = express();
 // we need to accept json data comming from the frontend
@@ -22,17 +21,19 @@ app.use("/api/chat",chatRoutes);
 app.use("/api/message",messageRoute);
 
 //  ---------------Deployment----------------
-// const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static("app/frontend/dist"));
-    app.get("*",(req,res)=>{
-        const filePath = path.resolve(__dirname,"app/frontend/dist","index.html");
-        res.sendFile(filePath);
-    })
-}else{
-    app.get('/',(req,res)=>{
-        res.send('API is running');
-    });
+const __dirname1 = path.resolve();
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
 //  ---------------Deployment----------------
