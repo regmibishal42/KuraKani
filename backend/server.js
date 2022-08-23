1f26aac8cdb16a332ea79cb0e75021d55457ed2d
Bishal Regmi <regmibishal42@gmail.com>
2022-08-23 10:16:24 +0545

    dir name sucks

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname1,"../frontend","dist")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
+        res.sendFile(path.resolve(__dirname,"/frontend","dist","index.html"));
     })
 }else{
     app.get('/',(req,res)=>{
