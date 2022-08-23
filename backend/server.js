1bb9577e0d6157661a1e47574648475b91279c08
Bishal Regmi <regmibishal42@gmail.com>
2022-08-23 09:41:55 +0545

    Ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoute = require('./routes/messageRoutes');
 const {notFound,errorHandler} = require('./middleware/errorMiddleware');
-const { emit } = require('./models/userModel');
+const path = require('path');
 
 const app = express();
 // we need to accept json data comming from the frontend
@@ -16,19 +16,32 @@ app.use(cors());
 dotenv.config();
 connectDb();
 
-app.get('/',(req,res)=>{
-    res.send('API is running');
-});
 app.use("/api/user",userRoutes);
 app.use("/api/chat",chatRoutes);
-app.use("/api/message",messageRoute)
+app.use("/api/message",messageRoute);
+
+//  ---------------Deployment----------------
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname1,"..","frontend","dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"..","frontend","dist","index.html"));
+    })
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is running');
+    });
+}
+
+//  ---------------Deployment----------------
+
 
 app.use(notFound);
 app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(3000,console.log(`Server is Listening on port ${port}`));
+const server = app.listen(port,console.log(`Server is Listening on port ${port}`));
 
 const io = require('socket.io')(server,{
     pingTimeout:60000,
--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -12,7 +12,7 @@ import io from 'socket.io-client';
 import Lottie from "react-lottie";
 import animationData from '../animations/typing.json';
  
-const ENDPOINT = "http://localhost:3000";
+const ENDPOINT = "https://kuraakani.herokuapp.com/";
 let socket,selectedChatCompare;
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
